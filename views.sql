eef07817beca6decdc3b5244ef00b9e7e5e7a723
Veerendranath Korrapati <vkpati20@gmail.com>
2020-11-18 20:45:22 -0600

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,3 +1,14 @@
 -- All views below!
 
 -- Annual Top Members
+
+
+
+
+-- Potential Member Customer
+create view Potential_Member_Customer("First Name", "Orders count")
+as
+select person.first_name, count(order_id)from person inner join customer on 
+person.person_id = customer.person_id inner join ORDERS on customer.person_id = ORDERS.from_customer
+where customer.is_member = false and (orders.on_date > CURRENT_DATE - INTERVAL '1 month')
+group by orders.from_customer, person.first_name having count(order_id) > 10
