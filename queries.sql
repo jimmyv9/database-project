f2bffeadfcdad267aa776ea3aa52e21d04c308e0
jimmyv9 <joaopvapv100@yahoo.com.br>
2020-11-25 19:25:56 -0600

    Complete and numbered queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,10 @@
+-- 1
 -- Find names of employees who are also members at the mall
 SELECT first_name, last_name 
 FROM (PERSON NATURAL JOIN EMPLOYEE)
 WHERE EMPLOYEE.is_member = TRUE;
 
+-- 2
 -- Find names of all cashiers, and their supervisors
 SELECT first_name, last_name,
 (SELECT first_name
@@ -27,10 +29,12 @@ FROM (PERSON NATURAL JOIN FLOOR_STAFF);
 SELECT first_name, last_name
 FROM (PERSON NATURAL JOIN MANAGER);
 
+-- 3
 -- Find average of orders made by potential member customers
 SELECT ROUND(AVG("Orders count"), 2)
 FROM POTENTIAL_MEMBER_CUSTOMER;
 
+-- 4
 -- Find all customers who purchased the most popular product
 SELECT first_name, last_name
 FROM PERSON
@@ -40,16 +44,7 @@ WHERE person_id IN(
 	WHERE product_id IN (SELECT product_id FROM Popular_Product)
 );
 
-
--- Find employees who became members after becoming employed
-SELECT first_name, last_name
-FROM PERSON
-WHERE person_id IN (
-	SELECT person_id
-	FROM (EMPLOYEE JOIN ALLMEMBERS ON person_id = member_id) NATURAL JOIN CARD
-	WHERE issued > start_date
-);
-
+-- 5
 -- Find employees who became a member one month after starting work
 SELECT first_name, last_name
 FROM person
@@ -59,15 +54,13 @@ WHERE person_id IN (
 	WHERE issued > start_date AND issued < start_date + INTERVAL '1 month'
 );
 
+-- 6
 -- Find names members who bring the most guests
 SELECT person.first_name, person.last_name, COUNT(*) AS number_of_guests
 FROM PERSON JOIN GUEST ON person_id = member_id
 GROUP BY person.first_name, person.last_name
 ORDER BY COUNT(*) DESC
 LIMIT 1;
-
-
-
 
 
 
