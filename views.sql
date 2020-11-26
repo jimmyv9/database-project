e2e68e0786165eaf82d172687917a063a7da644a
Veerendranath Korrapati <vkpati20@gmail.com>
2020-11-25 19:51:11 -0600

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,7 +1,7 @@
 -- All views below!
 
 -- Annual Top Members
-CREATE VIEW Annual_Top_Members AS
+CREATE VIEW Annual_Top_Members(First_name, Last_name) AS
 SELECT first_name, last_name,
 (SELECT issued
  FROM CARD NATURAL JOIN ALLMEMBERS
@@ -25,29 +25,29 @@ WHERE product_id IN (SELECT product_id
 
 
 -- Potential Member Customer
-create view Potential_Member_Customer("First Name", "Orders count")
+create view Potential_Member_Customer(First_name, Order_count)
 as
 select person.first_name, count(order_id)from person inner join customer on 
 person.person_id = customer.person_id inner join ORDERS on customer.person_id = ORDERS.from_customer
-where customer.is_member = false and (orders.on_date > date('2020-01-01') - INTERVAL '1 month')
+where customer.is_member = false and (orders.on_date > CURRENT_DATE - INTERVAL '1 month')
 group by orders.from_customer, person.first_name having count(order_id) > 10
 
 
 -- Gold Store
-CREATE VIEW Gold_Store
-SELECT sname, COUNT(DISTINCT orders.from_customer)
+CREATE VIEW Gold_Store2(Store_id, Store_name, Distinct_customers) as
+SELECT store_id, sname, COUNT(DISTINCT orders.from_customer)
 FROM store join orders on orders.at_store = store.store_id
-WHERE (orders.on_date > date('2020-01-01') - INTERVAL '1 year')
-GROUP BY store.sname 
+WHERE (orders.on_date > CURRENT_DATE - INTERVAL '1 year')
+GROUP BY store.sname, store.store_id
 ORDER BY COUNT(DISTINCT from_customer) desc limit 1
 
 
 -- Top Quarter Cashier
-CREATE VIEW Top_Quarter_Cashier AS
+CREATE VIEW Top_Quarter_Cashier(First_name, Last_name, Number_of_orders) AS
 SELECT first_name, last_name, COUNT(*)
 FROM CASHIER JOIN PYMT_INFO ON cashier.person_id = pymt_info.cashier_id
             NATURAL JOIN PERSON
-WHERE on_date > date('2020-01-01') - INTERVAL '3 month'
+WHERE on_date > CURRENT_DATE - INTERVAL '3 month'
 GROUP BY first_name, last_name
 ORDER BY COUNT(*) DESC
 LIMIT 1;
